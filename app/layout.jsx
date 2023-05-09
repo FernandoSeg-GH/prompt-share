a47de7b46b573c47ebaaa6151a7512be5a4d55e4
Your Name <you@example.com>
2023-05-09 15:33:22 -0300

    updates

--- app/layout.jsx
+++ app/layout.jsx
@@ -4,7 +4,7 @@ import Nav from "@components/Nav";
 import Provider from "@components/Provider";
 
 export const metadata = {
-  title: "Promptopia",
+  title: "Promp-Share",
   description: "Discover & Share AI Prompts",
 };
 
